57ba12b25b6db337ab7d2103aed46c6191cc6db1
Андрей <andrey.buharov@gmail.com>
2026-09-23 09:06:27 +0300

    Добавил номер версии.

--- init.php
+++ init.php
@@ -1,11 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-/**
- * Module meta.
- * Used by order module views at the bottom of the page.
- */
 
-// Bump this value when you change the order module.
-define('AUDITLOG_MODULE_VERSION', '1.0.1');
+defined('AUDITLOG_VERSION') OR define('AUDITLOG_VERSION', '1.0.1');
 
 	
 Kohana::$config->load('menu')
